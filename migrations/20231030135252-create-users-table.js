37bc720e2aa2cec956c2f47505e9c14e5e3840a4
hounsounon-anselme <ahounsounnon@gmail.com>
2023-11-04 01:47:51 +0100

    modify user and create react

--- migrations/20231030135252-create-users-table.js
+++ migrations/20231030135252-create-users-table.js
@@ -40,11 +40,6 @@ module.exports = {
                 unique: true,
 
             },
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 2240e17ae5193e7b8ae00b9e5c6f52b588733b59
             phone: {
                 type: Sequelize.STRING,
                 allowNull: false,
